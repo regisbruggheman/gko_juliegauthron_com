bb97ea8783f7f8bba7bba4db64bbdb3a3298ebda
Regis Bruggheman <regis.bruggheman@gmail.com>
2016-11-06 14:10:51 +0100

    bump

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150903104248) do
+ActiveRecord::Schema.define(:version => 20161106130620) do
 
   create_table "assets", :force => true do |t|
     t.integer  "site_id"
@@ -220,6 +220,7 @@ ActiveRecord::Schema.define(:version => 20150903104248) do
     t.integer  "image_height"
     t.string   "image_uid"
     t.string   "video_url"
+    t.string   "author"
   end
 
   create_table "inquiries", :force => true do |t|
